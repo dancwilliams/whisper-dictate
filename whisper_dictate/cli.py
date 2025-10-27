d33ec473953d3b239137be464cc28c5c95ebda5e
Dan C Williams <dan.github@mail.threatfax.com>
2025-10-27 11:26:39 -0500

    updated to use pyinstaller as well as got CUDA working with local packages

--- whisper_dictate/cli.py
+++ whisper_dictate/cli.py
@@ -1,8 +1,28 @@
+import os, importlib
+import sys
+from pathlib import Path
+
+def set_cuda_paths():
+    venv_base = Path(sys.executable).parent.parent
+    nvidia_base_path = venv_base / 'Lib' / 'site-packages' / 'nvidia'
+    cuda_path = nvidia_base_path / 'cuda_runtime' / 'bin'
+    cublas_path = nvidia_base_path / 'cublas' / 'bin'
+    cudnn_path = nvidia_base_path / 'cudnn' / 'bin'
+    paths_to_add = [str(cuda_path), str(cublas_path), str(cudnn_path)]
+    env_vars = ['CUDA_PATH', 'CUDA_PATH_V12_4', 'PATH']
+    
+    for env_var in env_vars:
+        current_value = os.environ.get(env_var, '')
+        new_value = os.pathsep.join(paths_to_add + [current_value] if current_value else paths_to_add)
+        os.environ[env_var] = new_value
+
+set_cuda_paths()
+
 import argparse
 import ctypes
 import ctypes.wintypes
 import queue
-import sys
+
 import threading
 import time
 from typing import Optional
@@ -16,6 +36,7 @@ except Exception:
 import numpy as np
 import pyperclip
 import sounddevice as sd
+
 from faster_whisper import WhisperModel
 
 # NEW: OpenAI-compatible client
--- whisper_dictate/gui.py
+++ whisper_dictate/gui.py
@@ -1,12 +1,31 @@
 # whisper_dictate/gui.py
 # GUI dictation with optional LLM cleanup (OpenAI-compatible endpoint, e.g., LM Studio)
 
+import os, importlib
+import sys
+from pathlib import Path
+
+def set_cuda_paths():
+    venv_base = Path(sys.executable).parent.parent
+    nvidia_base_path = venv_base / 'Lib' / 'site-packages' / 'nvidia'
+    cuda_path = nvidia_base_path / 'cuda_runtime' / 'bin'
+    cublas_path = nvidia_base_path / 'cublas' / 'bin'
+    cudnn_path = nvidia_base_path / 'cudnn' / 'bin'
+    paths_to_add = [str(cuda_path), str(cublas_path), str(cudnn_path)]
+    env_vars = ['CUDA_PATH', 'CUDA_PATH_V12_4', 'PATH']
+    
+    for env_var in env_vars:
+        current_value = os.environ.get(env_var, '')
+        new_value = os.pathsep.join(paths_to_add + [current_value] if current_value else paths_to_add)
+        os.environ[env_var] = new_value
+
+set_cuda_paths()
+
 import ctypes
 import ctypes.wintypes
 import threading
 import time
 import queue
-import sys
 from typing import Optional
 
 try:
